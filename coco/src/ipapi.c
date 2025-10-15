cf4d9b7f2880613fb63c3a2fdd9d240f6b6a9a80
Rich Stephens <richswww@gmail.com>
2025-10-15 13:32:08 -0400

    Change to use 4x8 font, some code formatting

--- coco/src/ipapi.c
+++ coco/src/ipapi.c
@@ -9,34 +9,39 @@
 #include "fujinet-network.h"
 #include "weatherdefs.h"
 #include "ipapi.h"
+#include "gfx.h"
 
 extern int	err;
 
 char ip_url[] = "N:http://ip-api.com/json/?fields=status,city,countryCode,lon,lat";
 
-
 //
-// get location info from ip 
+// get location info from ip
 // returns true: call is success
 //         false:     is not success
 //
-void get_location(LOCATION *loc) {
-	char	buf[LINE_LEN];
-	char	message[LINE_LEN];
+void get_location(LOCATION *loc)
+{
+	char buf[LINE_LEN];
+	char message[LINE_LEN];
 
 	err = network_open(ip_url, OPEN_MODE_READ, OPEN_TRANS_NONE);
-    handle_err("ip-api open");
+	handle_err("ip-api open");
 
-    err = network_json_parse(ip_url);
-    handle_err("ip-api parse");
+	err = network_json_parse(ip_url);
+	handle_err("ip-api parse");
+
+	memset(buf, 0, LINE_LEN);
 
 	network_json_query(ip_url, "/status", buf);
-	if (strcmp(buf, "success") != 0) {
+	if (strcmp(buf, "success\n") != 0)
+	{
+		memset(buf, 0, LINE_LEN);
 		network_json_query(ip_url, "/message", buf);
 		network_close(ip_url);
 		sprintf(message, "ip-api(%s)", buf);
-		err = 0xff;					// set unknown error 
-    	handle_err(message);
+		err = 0xff; // set unknown error
+		handle_err(message);
 	}
 
 	network_json_query(ip_url, "/city", loc->city);
